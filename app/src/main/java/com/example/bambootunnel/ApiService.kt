b96abcab8e01666b450d54ab14afc442a5a2d2f7
Alexandre Barrymore <131294388+twiin0@users.noreply.github.com>
2025-07-09 11:34:51 +0100

    Delete app directory

--- app/src/main/java/com/example/bambootunnel/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bambootunnel
-
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-interface ApiService {
-    @GET("browse")
-    suspend fun browse(@Query("path") path: String): List<FileItem>
-}
