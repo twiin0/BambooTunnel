fe8c61a5f0753d8ba8ba00d9539588a90c6bdad1
Alexandre Barrymore <twinos@Alexandres-iMac.local>
2025-07-10 10:38:30 +0100

    Committing java server source to git

---
+++ server/src/test/java/com/bt/bambooTunnel/BambooTunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.bambooTunnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooTunnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
